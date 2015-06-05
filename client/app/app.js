61757b12c4033194c29ba5fb2b18093fb1a1e073
Daniel Rios <ieldanr@gmail.com>
2015-06-05 04:18:45 -0500

    remove mobile

--- client/app/app.js
+++ client/app/app.js
@@ -10,7 +10,7 @@ angular.module('moniNodeApp', [
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
 
     $urlRouterProvider
-      //.otherwise('/');
+      .otherwise('/');
 
     $locationProvider.html5Mode(true);
     $httpProvider.interceptors.push('authInterceptor');
