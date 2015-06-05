b4b0c28d165db8237bd763b8ae06336e62a5f16c
Daniel Rios <ieldanr@gmail.com>
2015-06-05 06:00:56 -0500

    adds user to created text polls

--- client/app/askText/askText.controller.js
+++ client/app/askText/askText.controller.js
@@ -1,14 +1,13 @@
 'use strict';
 
 angular.module('moniNodeApp')
-  .controller('AskTextCtrl', function ($scope, $http, $location) {
-    $scope.message = 'Hello';
+  .controller('AskTextCtrl', function ($scope, $http, $location, Auth) {
     $scope.create = function(poll) {
         var json_poll = {
           question: poll.question,
           option1: poll.option1,
           option2: poll.option2,
-          user: "anon",
+          user: Auth.getCurrentUser()._id,
           option1Count: 0,
           option2Count: 0,
           limitType: "vote",
