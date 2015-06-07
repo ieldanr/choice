7efff079ed6add94df3d3abb62d2f8a401b8b2c1
Daniel Rios <ieldanr@gmail.com>
2015-06-07 03:37:35 -0500

    allow any facebook account

--- server/api/user/user.model.js
+++ server/api/user/user.model.js
@@ -73,21 +73,6 @@ UserSchema
     return hashedPassword.length;
   }, 'Password cannot be blank');
 
-// Validate email is not taken
-UserSchema
-  .path('email')
-  .validate(function(value, respond) {
-    var self = this;
-    this.constructor.findOne({email: value}, function(err, user) {
-      if(err) throw err;
-      if(user) {
-        if(self.id === user.id) return respond(true);
-        return respond(false);
-      }
-      respond(true);
-    });
-}, 'The specified email address is already in use.');
-
 var validatePresenceOf = function(value) {
   return value && value.length;
 };
